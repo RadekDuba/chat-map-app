ce1d35f432fda7e6a77ae06f6ccfecfa063be288
RadekDuba <radek.duba@maptiler.com>
2025-04-09 00:15:27 +0200

    Build: Explicitly include @maptiler/sdk in optimizeDeps

--- mapchat-frontend/vite.config.js
+++ mapchat-frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@maptiler/sdk'], // Explicitly include the SDK for pre-bundling
+  },
   server: {
     proxy: {
       // Proxy API requests to the deployed Cloudflare Worker
